805c358fb5d598e758ef9bfe8add2454b8e23d72
PAQUIS Gerald <gerald.paquis@sensimedia.org>
2019-09-20 11:52:54 +0200

    Update generate_user.py

--- generate_user.py
+++ generate_user.py
@@ -11,17 +11,23 @@ dnsfile = open('/etc/dnsmasq.d/sensimedia.dns', 'r')
 for line in dnsfile:
         if (line.find('dhcp-host') > -1):
                 hostline = line.split(",")[0]
-                macbase = hostline.replace("dhcp-host=","")
-                myline = macbase.replace(":","")
+                hostline = hostline.split("=")[1]
+                vlanid = line.split(",")[1]
+                vlanid = vlanid.split(":")[-1]
+                vlanid = vlanid.replace("vlan","")
+                myline = hostline.replace(":","")
                 u = myline.upper()
+                myline= u.lower()
                 myresult= "-".join(["%s%s" % (u[i], u[i+1]) for i in range(0,12,2)])
 
                 authorizedmacs.write (str(myresult)+'\n')
-                authorizedmacs.write ('\t'+'Reply-Message = "Device with MAC Address %{Calling-Station-Id} authorized for network access"\n')
-                authorizeconf.write (str(myline) + '\tCleartext-Password :=\t' + '\"' + str(myline) +'\"\n')
+                authorizedmacs.write ('\t'+'Reply-Message = "Device with MAC Address %{Calling-Station-Id}
+ authorized for network access"\n')
+                authorizeconf.write (str(myline) + '\tCleartext-Password :=\t' + '\"' + str(myline) +'\"\n
+')
                 authorizeconf.write ('\t\t' + 'Tunnel-Type = "VLAN",\n')
                 authorizeconf.write ('\t\t' + 'Tunnel-Medium-Type = "IEEE-802",\n')
-                authorizeconf.write ('\t\t' + 'Tunnel-Private-Group-ID = "1",\n')
+                authorizeconf.write ('\t\t' + 'Tunnel-Private-Group-ID = "'+ str(vlanid) +'"\n')
                 authorizeconf.write ('\t\t' + 'Auth-Type := local\n')
                 authorizeconf.write ('\n')
 
